2b99483403aa81f6aec2863c9fb0cee5307f959c
izumiano <elibrine@gmail.com>
2024-09-10 22:05:43 +0200

    add rouletteWheel

--- main.js
+++ main.js
@@ -15,6 +15,14 @@ async function request(url) {
 	}
 }
 
+function pxToRem(px) {
+  return px / parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
+
+function remToPx(rem) {
+	return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
+
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max + 1 - min)) + min;
 }
@@ -30,19 +38,22 @@ function disableButton(button) {
 }
 
 function rollRoulette() {
-	if (rouletteButton.disabled) return;
+	console.log("now");
+	let mapsToRoll = getMapsToRoll(selectedTiers, goldenList);
+	getRandomMap(mapsToRoll);
+}
 
-  let mapsToRoll = getMapsToRoll(selectedTiers, goldenList);
-	
+function getRandomMap(mapsToRoll) {
 	if (mapsToRoll.length <= 0) return;
 
 	const randomMap = mapsToRoll[getRandomInt(0, mapsToRoll.length - 1)];
 	campaignNameObject.innerText = randomMap.name === randomMap.campaignName ? "" : randomMap.campaignName;
 	const challenge = randomMap.challenges[0]
 	mapNameObject.innerHTML =
-    "<a href='https://www.goldberries.net/challenge/" + challenge.id + "' target='_blank' rel='noopener noreferrer'>" +
-    randomMap.name +
-    (challenge.requires_fc ? " [FC]" : "") +
+    "<a href='https://www.goldberries.net/challenge/" +
+    challenge.id +
+    "' target='_blank' rel='noopener noreferrer'>" +
+    randomMap.name + (challenge.requires_fc ? " [FC]" : "") +
     "</a>";
 	mapSegmentObject.innerText = challenge.label !== null ? challenge.label : "";
 	mapTierObject.innerText = challenge.difficulty.name;
@@ -51,22 +62,12 @@ function rollRoulette() {
 }
 
 function getMapsToRoll(tiers, tierSortedMaps) {
-	if (tiers.length <= 0) {
-		tiers = [
-			"Tier 0",
-			"Tier 1",
-			"Tier 2",
-			"Tier 3",
-			"Tier 4",
-			"Tier 5",
-			"Tier 6",
-			"Tier 7",
-			"Standard",
-		]
+	if (tiers === null || tiers.length <= 0 || tierSortedMaps === null || tierSortedMaps.length <= 0) {
+		return [];
 	}
 
 	let mapsToRoll = [];
-	tiers.forEach((tier) => {
+	tiers.forEach(tier => {
     if (tier in tierSortedMaps) {
       mapsToRoll = mapsToRoll.concat(tierSortedMaps[tier]);
     } else {
@@ -120,10 +121,108 @@ function selectInDropdown() {
 		selectedTiers.push(button.value);
 	}
 
+	addMapsToRoulette()
+
 	console.log(selectedTiers);
 }
 
-const rouletteButton = document.getElementById("rouletteButton");
+function resizeFontToFit(htmlObj, width, height) {
+	const compStyle = getComputedStyle(htmlObj);
+	const fontSize = compStyle.fontSize;
+	const styleWidth = compStyle.width;
+	htmlObj.style.width = "auto";
+
+	htmlObj.style.fontSize = parseFloat(fontSize) - 1 + "px";
+
+	if (htmlObj.clientWidth > width || htmlObj.clientHeight > height) {
+    resizeFontToFit(htmlObj, width, height);
+  }
+	htmlObj.style.width = styleWidth;
+}
+
+function addMapsToRoulette() {
+	const shallowChildrenCopy = [...rouletteContainer.children];
+	for (const child of shallowChildrenCopy) {
+    if (child.tagName === "P") {
+      rouletteContainer.removeChild(child);
+    }
+  }
+
+	// let maps = [
+  //   {
+  //     name: "Waterbear Mountain",
+  //     challenges: [
+  //       {
+  //         requires_fc: true,
+  //         label: null,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     name: "Pinball purgatory",
+  //     challenges: [
+  //       {
+  //         requires_fc: false,
+  //         label: null,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     name: "GMHS",
+  //     challenges: [
+  //       {
+  //         requires_fc: false,
+  //         label: null,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     name: "LXVI - Venus",
+  //     challenges: [
+  //       {
+  //         requires_fc: false,
+  //         label: null,
+  //         challenge: {
+  //           label: "start - end",
+  //         },
+  //       },
+  //     ],
+  //   },
+  // ];
+	const maps = getMapsToRoll(selectedTiers, goldenList);
+
+	let challengeCount = 0;
+	maps.forEach(map => {
+		map.challenges.forEach(_ => {
+			challengeCount++;
+		})
+	});
+
+	let count = 0;
+	maps.forEach(map => {
+		map.challenges.forEach(challenge => {
+			let p = document.createElement("p");
+			p.innerHTML =
+				map.name +
+				(challenge.requires_fc ? " [FC]" : "") + " "
+				+ (challenge.label !== null ? "[" + challenge.label + "]" : "") + " "
+			// + challenge.difficulty.name;
+			
+			
+			rouletteContainer.appendChild(p);
+			p.style = "--nth-child: " + count;
+			resizeFontToFit(p, remToPx(15) - 70, 20 * Math.PI * 15 / challengeCount);
+			count++;
+		})
+	});
+	rouletteContainer.style = "--childRotation: " + (challengeCount !== 0 ? 360 / challengeCount : 10) + "deg";
+}
+
+//--------------------
+
+const rouletteContainer = document.getElementById("rouletteContainer");
+const rouletteLoadIcon = document.getElementById("rouletteLoadIcon");
+const goldberryImg = document.getElementById("goldberryImg");
 
 const tierDropdown = document.getElementById("tierDropdown");
 const tierDropdownPopup = document.getElementById("tierDropdownPopup");
@@ -133,18 +232,34 @@ const mapNameObject = document.getElementById("mapName");
 const mapSegmentObject = document.getElementById("mapSegment");
 const mapTierObject = document.getElementById("mapTier");
 
-addEventListener("resize", _ => setTierDropdownPopoverWidth());
-
 let goldenList = null;
 let campaignList = [];
 let selectedTiers = [];
+
+//----------------------
+
+setTierDropdownPopoverWidth();
+addEventListener("resize", (_) => setTierDropdownPopoverWidth());
+
+// let count = 1;
+// rouletteContainer.style = "--childRotation: " + 360/count + "deg";
+// for (let i = 0; i < count; i++){
+// 	const p = document.createElement("p");
+// 	p.innerHTML = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+// 	rouletteContainer.appendChild(p);
+// 	p.style = "--nth-child: " + i;
+// 	resizeFontToFit(p, remToPx(15) / 4);
+// }
+
+// addMapsToRoulette();
 
 request("https://goldberries.net/api/lists/golden-list?archived=true&arbitrary=true")
 	.then(value => {
 		campaignList = value;
 		goldenList = getGoldenListFromCampaigns(campaignList);
-		console.log(goldenList);
-		enableButton(rouletteButton, "Roll Roulette");
+		rouletteLoadIcon.classList.add("hidden");
+		goldberryImg.classList.remove("hidden");
+		addMapsToRoulette();
 	})
 	.catch(err => {
 		console.error(err);
